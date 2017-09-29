abb984ddac25e97cdf8f2389d239a73d83863f59
GATKARIPOOJA <31990101+GATKARIPOOJA@users.noreply.github.com>
2017-09-29 14:21:07 +0530

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,16 @@
+
+public class Manager {
+	void initialorder()
+	{
+		System.out.println("Display the food item for initialorder");
+	}
+ void receivecomplaint()
+ {
+	 System.out.println("It receives the complaint from the customer");
+ }
+ void receivebill()
+ {
+	 System.out.println("Manager receives the bill from customer");
+ }
+}
+
